eb779bdc9f5640d89e18a4f44507352f27271536
Ahmed Elzohry <aelzohry2010@gmail.com>
2019-07-22 01:32:46 +0200

    better-approach by using custom flow layout

---
+++ PeekCollectionView/SnappingCollectionViewLayout.swift
@@ -0,0 +1,50 @@
+//
+//  SnappingCollectionViewLayout.swift
+//  PeekCollectionView
+//
+//  Created by Ahmed Elzohry on 7/22/19.
+//  Copyright © 2019 Abdelrahman Mohamed. All rights reserved.
+//
+
+import UIKit
+
+/* check
+    https://stackoverflow.com/questions/33855945/uicollectionview-snap-onto-cell-when-scrolling-horizontally
+    https://developer.apple.com/documentation/uikit/uicollectionviewlayout/1617729-targetcontentoffset
+ */
+class SnappingCollectionViewLayout: UICollectionViewFlowLayout {
+    
+    override init() {
+        super.init()
+        
+        scrollDirection = .horizontal
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        scrollDirection = .horizontal
+    }
+    
+    override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
+        guard let collectionView = collectionView else {
+            return super.targetContentOffset(forProposedContentOffset: proposedContentOffset, withScrollingVelocity: velocity)
+        }
+        
+        var offsetAdjustment = CGFloat.greatestFiniteMagnitude
+        let horizontalOffset = proposedContentOffset.x + collectionView.contentInset.left
+        
+        let targetRect = CGRect(x: proposedContentOffset.x, y: 0, width: collectionView.bounds.size.width, height: collectionView.bounds.size.height)
+        
+        let layoutAttributesArray = super.layoutAttributesForElements(in: targetRect)
+        
+        layoutAttributesArray?.forEach({ (layoutAttributes) in
+            let itemOffset = layoutAttributes.frame.origin.x
+            if fabsf(Float(itemOffset - horizontalOffset)) < fabsf(Float(offsetAdjustment)) {
+                offsetAdjustment = itemOffset - horizontalOffset
+            }
+        })
+        
+        return CGPoint(x: proposedContentOffset.x + offsetAdjustment, y: proposedContentOffset.y)
+    }
+}
